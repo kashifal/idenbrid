b9f52db6b0963c35a1b9c2c34089fe0e67b5745a
kashifal <captainkashi4@gmail.com>
2022-02-06 16:09:31 +0500

    commited

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+function display(n)
+{
+    if(n == 50)
+    {
+        return alert('51')
+    }else if(n == 51)
+    {
+        return alert('50')
+    }
+}
+
+
+const x = display(prompt('Enter Your Number? '));
